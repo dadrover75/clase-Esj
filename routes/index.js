20c1fe94c48a5a98d8ceb4dbcad323437fb426de
John DoeDiana <dadrover75@hotmail.com>
2021-12-02 01:22:02 -0300

    DA 4 paso2 en progreso

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render("../views/index.ejs");;
 });
+/* GET detalle Menu. */
+router.get('/detalle/', function(req, res, next) {
+  res.render("../views/detalleMenu.ejs");;
+});
+
+/* GET detalle producto. */
+router.get('/detalle/:id', function(req, res, next) {
+  res.render("../views/detalleProducto.ejs");;
+});
 
 module.exports = router;
